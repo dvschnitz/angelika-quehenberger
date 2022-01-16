4e0b24432c98b234bbd3cb969a685ed1558be25b
David Schnitzhofer <dvschnitz@gmail.com>
2022-01-16 17:31:38 +0100

    Adapted menu toggle behaviour

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,22 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+const lastOpenedKey = 'lastOpenedParent';
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
+    menuTrigger.classList.toggle('is-active');
+    body.classList.toggle('lock-scroll');
+
+    var lastOpenedNodeId = localStorage.getItem(lastOpenedKey);
+    if (lastOpenedNodeId) {
+        toggleSubMenu(lastOpenedNodeId);
+    }
 }
 
 toggleSubMenu = function(id) {
     var childrenToToggle = document.querySelectorAll('.child-' + id);
-    console.log('name', name, childrenToToggle);
+    localStorage.setItem(lastOpenedKey, id);
     for (let i = 0; i < childrenToToggle.length; i++) {
         if (childrenToToggle[i].style.display === 'none') {
             childrenToToggle[i].style.display = "block";
