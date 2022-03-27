792246a6be8f8d74ae319a12d6677940018083d6
David Schnitzhofer <dvschnitz@gmail.com>
2022-03-27 21:00:57 +0200

    Implemented slideshow

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,12 @@ var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
 const lastOpenedKey = 'lastOpenedParent';
 
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") {
+        slideshow();
+    }
+});
+
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active');
@@ -22,6 +28,27 @@ toggleSubMenu = function(id) {
             childrenToToggle[i].style.display = "block";
         } else {
             childrenToToggle[i].style.display = "none";
+        }
+    }
+}
+
+slideshow = function() {
+    var containsSlideShow = document.querySelectorAll('#image-2');
+    if (containsSlideShow && containsSlideShow.length > 0) {
+        var elements = document.querySelectorAll('.image-slideshow');
+        var numberOfElements = elements.length;
+        var previousElement;
+        for (var i = 0; i < 100; i++) {
+            (function(ind) {
+                setTimeout(function(){
+                    if (previousElement) {
+                        previousElement.style.display = 'none';
+                    }
+                    previousElement = elements[ind % numberOfElements];
+                    previousElement.style.display = 'inline';
+
+                }, 1000 + (5000 * ind));
+            })(i);
         }
     }
 }
